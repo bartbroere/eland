218c1b44038d8a6bbf4f3ca368c2eb7cc057ca99
Bart Broere <mail@bartbroere.eu>
2024-02-13 14:23:53 +0000

    Bump for fork release

--- eland/_version.py
+++ eland/_version.py
@@ -18,7 +18,7 @@
 __title__ = "bartbroere_eland"
 __description__ = "[Development fork!] Python Client and Toolkit for DataFrames, Big Data, Machine Learning and ETL in Elasticsearch"
 __url__ = "https://github.com/elastic/eland"
-__version__ = "8.12.1"
+__version__ = "8.12.2"
 __author__ = "Steve Dodson"
 __author_email__ = "steve.dodson@elastic.co"
 __maintainer__ = "Elastic Client Library Maintainers"
